98d6806417313d198091538087f445cff948c964
DyotisianAnurag <anurag@dyotis.com>
2023-05-01 23:43:47 +0530

    Center alignment added

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ var Graph = function Graph(_ref) {
     width: "100%",
     height: "100%",
     border: "1px solid black",
-    viewBox: "-450 -250 900 500"
+    viewBox: "-600 -250 1200 500"
   }, restProps), _react["default"].createElement("g", {
     className: "_graphZoom"
   }, data.links.map(function (link, i) {
