fa020a59ba0e9c8b21480aceae593160d3204450
DyotisianAnurag <anurag@dyotis.com>
2023-05-01 15:51:48 +0530

    Tree position center aligned

--- src/index.js
+++ src/index.js
@@ -69,6 +69,8 @@ const Graph = ({
             width="100%"
             height="100%"
             {...restProps}
+            x="50%"
+            y="50%"
         >
              <g className="_graphZoom">
                  {
