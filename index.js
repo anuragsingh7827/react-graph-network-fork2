fa7060fd4c077b8ee6e5b00f3fa1024d27c6eb8f
DyotisianAnurag <anurag@dyotis.com>
2023-05-01 23:44:58 +0530

    Center aligned the tree

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ var Graph = function Graph(_ref) {
     width: "100%",
     height: "100%",
     border: "1px solid black",
-    viewBox: "-600 -250 1200 500"
+    viewBox: "-200 -250 400 500"
   }, restProps), _react["default"].createElement("g", {
     className: "_graphZoom"
   }, data.links.map(function (link, i) {
