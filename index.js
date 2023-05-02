6392942efdf0b9e4d76a8c7d33a1caec04eb2069
DyotisianAnurag <anurag@dyotis.com>
2023-05-02 23:10:48 +0530

    Center alignment added

--- index.js
+++ index.js
@@ -60,9 +60,10 @@ var Graph = function Graph(_ref) {
     .id(function (d) {
       return d.id;
     }) // This provide the id of a node
+    .distance(nodeDistance)
     .strength(1) // this makes the nodes movable
     .links(data.links) // and this the list of links
-    ).force("charge", (0, _d3Force.forceManyBody)().strength(-50)) // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+    ).force("charge", (0, _d3Force.forceManyBody)().theta([0]).strength(-50)) // This adds repulsion between nodes. Play with the -400 for the repulsion strength
     .on("tick", function () {
       return (0, _events.tick)(node, link);
     })
