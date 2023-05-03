2b73a1fb1cd274edc47bddfd0ca7099027efebff
DyotisianAnurag <anurag@dyotis.com>
2023-05-03 11:21:46 +0530

    Center alignment bug fix

--- index.js
+++ index.js
@@ -64,8 +64,6 @@ var Graph = function Graph(_ref) {
     .strength(1) // this makes the nodes movable
     .links(data.links) // and this the list of links
     ).force("charge", (0, _d3Force.forceManyBody)().distanceMax(700).strength(-50)) // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-    .force("x", (0, _d3Force.forceX()))
-    .force("y", (0, _d3Force.forceY()))
     .on("tick", function () {
       return (0, _events.tick)(node, link);
     })
