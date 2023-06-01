a4cf980ffd564fa0fee9bc11f0d874404d90dedc
DyotisianAnurag <anurag@dyotis.com>
2023-06-01 16:58:43 +0530

    Bug fixed

--- interactions.js
+++ interactions.js
@@ -19,38 +19,77 @@ var addZoom = function addZoom(svg, zoomDepth) {
     var svgWidth = svg._groups[0][0].clientWidth;
 
     var zoomed = function zoomed() {
-      svg.selectAll("._graphZoom").attr("transform", _d3Selection.event.transform);
+      svg
+        .selectAll("._graphZoom")
+        .attr("transform", _d3Selection.event.transform);
       var currentZoom = _d3Selection.event.transform.k;
       localStorage.setItem("currentZoom", currentZoom);
+      var currentTranslate = _d3Selection.event.transform;
+      localStorage.setItem(
+        "currentTranslate",
+        JSON.stringify(currentTranslate),
+      );
     };
 
     var zoom = (0, _d3Zoom.zoom)()
-      .extent([[0, 0], [svgWidth, svgHeight]])
+      .extent([
+        [0, 0],
+        [svgWidth, svgHeight],
+      ])
       .scaleExtent([1, zoomDepth])
       .on("zoom", zoomed);
 
     var drag = (0, _d3Drag.drag)()
-      .on("start", function() {
+      .on("start", function () {
         if (_d3Selection.event.sourceEvent.type !== "brush") {
           _d3Selection.event.sourceEvent.stopPropagation();
         }
       })
-      .on("drag", function() {
+      .on("drag", function () {
         if (_d3Selection.event.sourceEvent.type !== "brush") {
           svg.attr("transform", _d3Selection.event.transform);
         }
       });
 
-    var zoomIn = function() {
+    var zoomIn = function () {
       zoom.scaleBy(svg.transition().duration(500), 1.2);
       var currentZoom = zoom.scaleExtent()[1];
       localStorage.setItem("currentZoom", currentZoom);
+      repositionGraph();
     };
 
-    var zoomOut = function() {
+    var zoomOut = function () {
       zoom.scaleBy(svg.transition().duration(500), 0.8);
       var currentZoom = zoom.scaleExtent()[1];
       localStorage.setItem("currentZoom", currentZoom);
+      repositionGraph();
+    };
+
+    var repositionGraph = function () {
+      var transform = _d3Selection.zoomTransform(svg.node());
+      var currentZoom = transform.k;
+      var currentTranslate = [transform.x, transform.y];
+
+      var newTranslate = [
+        Math.min(
+          0,
+          Math.max(svgWidth - svgWidth * currentZoom, currentTranslate[0]),
+        ),
+        Math.min(
+          0,
+          Math.max(svgHeight - svgHeight * currentZoom, currentTranslate[1]),
+        ),
+      ];
+
+      svg
+        .transition()
+        .duration(500)
+        .call(
+          zoom.transform,
+          _d3Zoom.zoomIdentity
+            .translate(newTranslate[0], newTranslate[1])
+            .scale(currentZoom),
+        );
     };
 
     // Bind zoom in and zoom out functions to UI buttons
@@ -61,6 +100,27 @@ var addZoom = function addZoom(svg, zoomDepth) {
     var initialZoom = localStorage.getItem("currentZoom");
     if (initialZoom) {
       zoom.scaleTo(svg, initialZoom);
+    }
+
+    // Retrieve and set the initial translation from local storage
+    var initialTranslate = localStorage.getItem("currentTranslate");
+    if (initialTranslate) {
+      initialTranslate = JSON.parse(initialTranslate);
+      svg.call(
+        zoom.transform,
+        _d3Zoom.zoomIdentity.translate(
+          initialTranslate[0],
+          initialTranslate[1],
+        ),
+      );
+    } else {
+      // Center the graph on initial render
+      var centerX = svgWidth / 2;
+      var centerY = svgHeight / 2;
+      svg.call(
+        zoom.transform,
+        _d3Zoom.zoomIdentity.translate(centerX, centerY),
+      );
     }
 
     svg.call(zoom).call(drag);
