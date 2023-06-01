8c5a224a8817411801caaa29f9fe4bbb6a3b8962
DyotisianAnurag <anurag@dyotis.com>
2023-06-01 20:49:46 +0530

    Bug fixed

--- interactions.js
+++ interactions.js
@@ -18,48 +18,43 @@ var addZoom = function addZoom(svg, zoomDepth) {
     var svgWidth = svg._groups[0][0].clientWidth;
 
     var zoomed = function zoomed() {
-      svg.selectAll("._graphZoom").attr("transform", _d3Selection.event.transform);
+      svg
+        .selectAll("._graphZoom")
+        .attr("transform", _d3Selection.event.transform);
       var currentZoom = _d3Selection.event.transform.k;
       localStorage.setItem("currentZoom", currentZoom);
     };
 
     var zoom = (0, _d3Zoom.zoom)()
-      .extent([[0, 0], [svgWidth, svgHeight]])
+      .extent([
+        [0, 0],
+        [svgWidth, svgHeight],
+      ])
       .scaleExtent([1, zoomDepth])
       .on("zoom", zoomed);
 
     var drag = (0, _d3Drag.drag)()
-      .on("start", function() {
+      .on("start", function () {
         if (_d3Selection.event.sourceEvent.type !== "brush") {
           _d3Selection.event.sourceEvent.stopPropagation();
         }
       })
-      .on("drag", function() {
+      .on("drag", function () {
         if (_d3Selection.event.sourceEvent.type !== "brush") {
           svg.attr("transform", _d3Selection.event.transform);
         }
       });
 
-    var zoomIn = function() {
+    var zoomIn = function () {
       zoom.scaleBy(svg.transition().duration(500), 1.2);
       var currentZoom = zoom.scaleExtent()[1];
       localStorage.setItem("currentZoom", currentZoom);
-      resetCenter();
     };
 
-    var zoomOut = function() {
+    var zoomOut = function () {
       zoom.scaleBy(svg.transition().duration(500), 0.8);
       var currentZoom = zoom.scaleExtent()[1];
       localStorage.setItem("currentZoom", currentZoom);
-      resetCenter();
-    };
-
-    var resetCenter = function() {
-      var scale = zoom.scale();
-      var translateX = (svgWidth / 2) - (svgWidth / 2) * scale;
-      var translateY = (svgHeight / 2) - (svgHeight / 2) * scale;
-      var transform = _d3Selection.zoomIdentity.scale(scale).translate(translateX, translateY);
-      svg.transition().duration(500).call(zoom.transform, transform);
     };
 
     // Bind zoom in and zoom out functions to UI buttons
@@ -70,7 +65,6 @@ var addZoom = function addZoom(svg, zoomDepth) {
     var initialZoom = localStorage.getItem("currentZoom");
     if (initialZoom) {
       zoom.scaleTo(svg, initialZoom);
-      resetCenter();
     }
 
     svg.call(zoom).call(drag);
@@ -78,69 +72,6 @@ var addZoom = function addZoom(svg, zoomDepth) {
 
   return svg;
 };
-
-
-
-// var addZoom = function addZoom(svg, zoomDepth) {
-//   if (zoomDepth) {
-//     var svgHeight = svg._groups[0][0].clientHeight;
-//     var svgWidth = svg._groups[0][0].clientWidth;
-
-//     var zoomed = function zoomed() {
-//       svg
-//         .selectAll("._graphZoom")
-//         .attr("transform", _d3Selection.event.transform);
-//       var currentZoom = _d3Selection.event.transform.k;
-//       localStorage.setItem("currentZoom", currentZoom);
-//     };
-
-//     var zoom = (0, _d3Zoom.zoom)()
-//       .extent([
-//         [0, 0],
-//         [svgWidth, svgHeight],
-//       ])
-//       .scaleExtent([1, zoomDepth])
-//       .on("zoom", zoomed);
-
-//     var drag = (0, _d3Drag.drag)()
-//       .on("start", function () {
-//         if (_d3Selection.event.sourceEvent.type !== "brush") {
-//           _d3Selection.event.sourceEvent.stopPropagation();
-//         }
-//       })
-//       .on("drag", function () {
-//         if (_d3Selection.event.sourceEvent.type !== "brush") {
-//           svg.attr("transform", _d3Selection.event.transform);
-//         }
-//       });
-
-//     var zoomIn = function () {
-//       zoom.scaleBy(svg.transition().duration(500), 1.2);
-//       var currentZoom = zoom.scaleExtent()[1];
-//       localStorage.setItem("currentZoom", currentZoom);
-//     };
-
-//     var zoomOut = function () {
-//       zoom.scaleBy(svg.transition().duration(500), 0.8);
-//       var currentZoom = zoom.scaleExtent()[1];
-//       localStorage.setItem("currentZoom", currentZoom);
-//     };
-
-//     // Bind zoom in and zoom out functions to UI buttons
-//     _d3Selection.select("#zoom-in-button").on("click", zoomIn);
-//     _d3Selection.select("#zoom-out-button").on("click", zoomOut);
-
-//     // Retrieve and set the initial zoom level from local storage
-//     var initialZoom = localStorage.getItem("currentZoom");
-//     if (initialZoom) {
-//       zoom.scaleTo(svg, initialZoom);
-//     }
-
-//     svg.call(zoom).call(drag);
-//   }
-
-//   return svg;
-// };
 
 exports.addZoom = addZoom;
 
